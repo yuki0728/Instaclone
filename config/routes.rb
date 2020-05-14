ee812e35c2d2a8bbf49aa7cfc820fe1515556c6e
yuki <aitev.yy0728@gmail.com>
2020-05-14 14:46:35 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get  '/signup', to: 'users#new'
+  root   'pages#home'
+
+  get    '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resource :users
 end
